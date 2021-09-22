3f7e93bf3557a4a910d3252a94ea89f2369b31f6
Alberto <antonioalbertodsb@gmail.com>
2021-09-22 10:33:25 -0300

    Modulo 2

---
+++ modulo2/projeto/script.js
@@ -0,0 +1,5 @@
+var element = document.querySelector('body');
+
+element.addEventListener('click', function () {
+  element.innerHTML = "<div class='mensagem-classe'><h2 id=mensagem-id>Espero que tenham gostado!</h2></div>";
+});
---
+++ modulo2/variaveis-js/index.js
@@ -0,0 +1,7 @@
+var name = 'Antonio';
+var name = 'Alberto';
+
+let name2 = 'Antonio';
+name2 = 'Alberto';
+
+console.log(name, name2);
